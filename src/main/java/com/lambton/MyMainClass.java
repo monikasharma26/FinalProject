4dc93e6ec5a77bfc5743d80905058454805b798a
monika <monikasharma26081991@gmail.com>
2020-01-28 08:03:11 +0530

    [ikroop] world

--- src/main/java/com/lambton/MyMainClass.java
+++ src/main/java/com/lambton/MyMainClass.java
@@ -1,9 +1,12 @@
 package com.lambton;
 
+import java.util.Arrays;
+
 public class MyMainClass
 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World");
 
     }
 }
