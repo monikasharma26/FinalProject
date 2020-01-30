607c5c33fd8d202fe95f6fe0d2a19ec226d773e4
monika <monikasharma26081991@gmail.com>
2020-01-30 09:00:19 +0530

    [Monika] Gender Enum added

--- src/main/java/com/lambton/Person.java
+++ src/main/java/com/lambton/Person.java
@@ -1,4 +1,82 @@
 package com.lambton;
 
-public class Person {
+import sun.security.util.Password;
+
+import java.time.LocalDate;
+
+public abstract class Person
+{
+    public int id,age;
+    public  String fName,lName,mobileNumber,emailId,userName;
+    public Gender gender;
+    LocalDate birthDate;
+    public Password password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
 }
