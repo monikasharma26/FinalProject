80cd0667f20407131daa5024d102a0209cae0f2b
kritima <kritimakukreja5@gmail.com>
2020-01-30 19:58:23 -0500

    [KRITIMA] Interface IDisplay created

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -3,7 +3,7 @@ package com.lambton;
 import java.util.HashMap;
 
 public abstract class Vehicle {
-    
+
     private int vehicleIdentificationNumber;
     private String vehicleDescription;
     private String manufacturerName;
