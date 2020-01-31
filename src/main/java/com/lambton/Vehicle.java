a1669b048899b38d0535e9860af9a91ca443e104
kritima <kritimakukreja5@gmail.com>
2020-01-30 19:56:40 -0500

    [KRITIMA] Vehicle class variables created

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -4,5 +4,14 @@ import java.util.HashMap;
 
 public abstract class Vehicle {
     
+    private int vehicleIdentificationNumber;
+    private String vehicleDescription;
+    private String manufacturerName;
+    private boolean selfDrive;
+    private boolean isInsured;
+    private int noOfSeat;
+    private String insuranceProviderName;
+    private String fuelType;
+    HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
 
 }
